68b244e9318a4c6c39a98e2225f5c34b26dc6e57
Spencer H <SpencerHanson3141@gmail.com>
2024-06-20 23:10:01 -0600

    2neurons traj

---
+++ src/population_analysis/plotting/trajectory/trajectory_2neurons.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+from population_analysis.processors.nwb import NWBSession
+from population_analysis.processors.nwb.filters import BasicFilter
+import matplotlib.pyplot as plt
+
+
+def plot_2neuron_trajectory(sess, ufilt):
+    fig, main_axs = plt.subplots(2, 2)
+
+    for idx, resp_trial_filter in enumerate([BasicFilter(sess.probe_trial_idxs, sess.num_trials), BasicFilter(sess.mixed_trial_idxs, sess.num_trials)]):
+        axs = main_axs[:, idx]
+        for motdir, ax in zip([-1, 1], axs):
+            trial_filt = sess.trial_motion_filter(motdir).append(resp_trial_filter)
+            units = np.mean(sess.units()[ufilt.idxs()][:, trial_filt.idxs()], axis=1)
+            u1 = units[0]
+            u2 = units[1]
+
+            ax.scatter(u1[0], u2[0], marker="o", color="green", s=64, label="Startpoint")
+            ax.scatter(u1[-1], u2[-1], marker="o", color="red", s=64, label="Endpoint")
+            for i in range(1, len(u1)):
+                x, y = u1[i-1], u2[i-1]
+                dx = u1[i]-x
+                dy = u2[i]-y
+                rel_len = np.sqrt(dx*dx + dy*dy)
+                ax.arrow(x, y, dx, dy, length_includes_head=True, head_width=.02*rel_len, head_length=.05*rel_len, overhang=1, linestyle="dotted")
+                # ax.annotate("", xy=(x, y), xytext=(dx, dy), arrowprops=dict(arrowstyle="->"))
+            # ax.plot(u1, u2)
+            if idx == 0:
+                ax.set_ylabel(f"motion={motdir}")
+            ax.legend()
+
+        axs[1].set_xlabel("Unit 233 activity")
+        axs[1].set_ylabel("Unit 273 activity motion=1")
+
+    main_axs[0, 0].set_title("Extrasaccadic")
+    main_axs[0, 1].set_title("Perisaccadic")
+
+
+    plt.show()
+    pass
+
+
+def main():
+    filename = "2023-05-15_mlati7_output"
+    # matplotlib.use('Agg')  # Uncomment to suppress matplotlib window opening
+    sess = NWBSession("../../../../scripts", filename, "../graphs")
+
+    ufilt = BasicFilter([373, 233], sess.units().shape[1])
+    plot_2neuron_trajectory(sess, ufilt)
+
+    # ufilt = sess.unit_filter_qm().append(
+    #     sess.unit_filter_probe_zeta().append(
+    #         sess.unit_filter_custom(5, .2, 1, 1, .9, .4)
+    #     )
+    # )
+
+
+if __name__ == "__main__":
+    main()
